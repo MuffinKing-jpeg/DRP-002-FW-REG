673dc8818de728a19ef5821444874f8d1aeb09f4
Andrii Dobrus <58091875+MuffinKing-jpeg@users.noreply.github.com>
2025-02-13 23:42:48 +0200

    Sync

---
+++ Inc/core.h
@@ -0,0 +1,12 @@
+#ifndef __CORE_H
+#define __CORE_H
+
+#define CORE_RTC_SLEEP_PERIOD 99
+
+typedef enum{
+    CORE_OK,
+    CORE_ERR,
+    CORE_BUSY,
+}CORE_HandleTypeDef;
+
+#endif
--- Inc/main.h
+++ Inc/main.h
@@ -1,2 +1,11 @@
+#ifndef __MAIN_H
+#define __MAIN_H
+
 #include <stdint.h>
 #include "stm32g0xx.h"
+#include "rtc.h"
+#include "core.h"
+
+CORE_HandleTypeDef RTC_Init(uint8_t sleep_period);
+
+#endif
